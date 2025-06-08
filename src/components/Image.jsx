00b75ba440e9bdedc02b22d1f9cdc4bc22fe1b48
Ryan <ryzhangofficial@gmail.com>
2025-06-08 16:07:38 -0400

    Delete image

--- src/components/Image.jsx
+++
@@ -1,7 +0,0 @@
-export default function Image({ uploadedData }) {
-  if (!uploadedData?.url) return null;
-
-  return (
-    <img src={uploadedData.url} alt="Uploaded Protein" width="350" height="350"/>
-  );
-}
